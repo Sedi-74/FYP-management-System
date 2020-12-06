11ee8b2d391e4f349fd9f5a696c0f33f169b7d30
Sedi-74 <mpumemthembu.74@gmail.com>
2020-12-06 12:15:08 +0200

    Added coordinator login

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+include "database.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Final Year Project Management System</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class = "back">
+	<img src="img/b1(1).jpg" width="800">
+
+	<div class = "login">
+		<h1 class ="heading ">Coordinator Login</h1>
+
+		<div class = "log">
+
+
+
+<form methods="POST" action="">
+				
+<label>Username</label>
+<input type="text" name="aname" required class="input"><br>
+<label>Password</label>
+<input type="Password" name="apass" required class="input"><br>
+<button type="submit" class="btn" name=" login">Login Here</button>
+
+</form>
+			
+		</div>
+
+	</div>
+
+<div class ="footer">
+	
+<footer><p>Copy Right &copy; Final Year Project Management System</p></footer>
+
+
+</div>
+   
+
+
+</body>
+</html>
